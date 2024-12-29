2dee87eacd63a1be72675acf691ab8f3887d7c8d
sharpedge12 <75135658+sharpedge12@users.noreply.github.com>
2024-12-29 19:01:08 +0530

    Change - Added tests

--- h/StackFrame.h
+++ h/StackFrame.h
@@ -4,33 +4,32 @@
 using std::vector;
 
 #include "Type.h"
-//#include "Action.h"
+// #include "Action.h"
 
-extern void* globalFramePtr;
-extern void* stackPtr;
+extern void *globalFramePtr;
+extern void *stackPtr;
 
 class StackFrame
 {
 public:
 	void addMember(Type in);
-	
+
 	//	set the left and right input types (can only be done once)
 	void setInput(Type left, Type right);
-	
-	size_t getSize() {return frameSize;}
+
+	size_t getSize() { return frameSize; }
 	Type getLeftInType();
 	Type getRightInType();
 	size_t getLeftOffset();
 	size_t getRightOffset();
-	
+
 private:
-	
-	//Action actions; //must always be functionAction
+	// Action actions; //must always be functionAction
 	vector<Type> members;
-	size_t frameSize=0;
-	bool inputSet=false;
-	int leftInputIndex=-1;
-	int rightInputIndex=-1;
+	size_t frameSize = 0;
+	bool inputSet = false;
+	int leftInputIndex = -1;
+	int rightInputIndex = -1;
 	size_t leftInputOffset;
 	size_t rightInputOffset;
 };
